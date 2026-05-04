db17ae92f5cd1d1adfbcd95bbf68d011c0a61f64
pigsouza <vbenicio089@gmail.com>
2026-05-03 23:44:33 -0400

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -5,7 +5,7 @@ local Windows = NothingLibrary.new({
     Title = "zyt hub",
     Description = "Auto Catch Otimizado",
     Keybind = Enum.KeyCode.RightControl,
-    Logo = 'http://www.roblox.com/asset/?id=18898582662'
+    Logo = 'http://www.roblox.com/asset/?id=125361711744319'
 })
 
 local TabFrame = Windows:NewTab({
