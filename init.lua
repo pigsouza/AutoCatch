7a7395d5d9f066373234eb7175a443d25703ce52
pigsouza <vbenicio089@gmail.com>
2026-05-04 15:21:05 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,3 @@
-local githubUrl = "https://raw.githubusercontent.com/pigsouza/AutoCatch/bot-dsc/"
-
 getgenv().AutoCatchConfig = getgenv().AutoCatchConfig or {
     AutoCatch = false,
     SecretLuckyBlockOnly = false,
@@ -7,10 +5,16 @@ getgenv().AutoCatchConfig = getgenv().AutoCatchConfig or {
         Common = false, Rare = false, Epic = false,
         Legendary = false, Mythical = false, Secret = false,
         Boss = false, Divine = false
+    },
+   Blink = {
+        Islands = {
+            Cave = false,
+            Safari = false
+        },
+        TargetRarities = {
+            Common = false, Rare = false, Epic = false,
+            Legendary = false, Mythical = false, Secret = false,
+            Boss = false, Divine = false
+        }
     }
 }
-
-loadstring(game:HttpGet(githubUrl .. "farm.lua"))()
-loadstring(game:HttpGet(githubUrl .. "ui.lua"))()
-
-print("O script foi carregado.")
