8d07b939599a89c32e31b507681a777f3ff352f9
pigsouza <vbenicio089@gmail.com>
2026-05-03 23:28:05 -0400

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -1,1 +1,50 @@
+local NothingLibrary = loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/3345-c-a-t-s-u-s/NOTHING/main/source.lua'))()
+local Notification = NothingLibrary.Notification()
 
+local Windows = NothingLibrary.new({
+    Title = "zyt hub",
+    Description = "Auto Catch Otimizado",
+    Keybind = Enum.KeyCode.RightControl,
+    Logo = 'http://www.roblox.com/asset/?id=18898582662'
+})
+
+local TabFrame = Windows:NewTab({
+    Title = "Auto Farm",
+    Description = "Selecione",
+    Icon = "rbxassetid://7733960981"
+})
+
+local MainSection = TabFrame:NewSection({ Title = "Controle Principal", Icon = "rbxassetid://7743869054", Position = "Left" })
+local RaritySection = TabFrame:NewSection({ Title = "Filtro de Raridades", Icon = "rbxassetid://7733964719", Position = "Right" })
+
+MainSection:NewToggle({
+    Title = "Ativar Auto Catch",
+    Default = false,
+    Callback = function(state)
+        getgenv().AutoCatchConfig.AutoCatch = state
+        if state then
+            Notification.new({
+                Title = "Farm Iniciado",
+                Description = "Buscando pets selecionados...",
+                Duration = 3,
+                Icon = "rbxassetid://8997385628"
+            })
+        end
+    end,
+})
+
+local raritiesToCreate = {"Common", "Rare", "Epic", "Legendary", "Mythical", "Secret", "Boss", "Divine"}
+
+for _, rarity in ipairs(raritiesToCreate) do
+    RaritySection:NewToggle({
+        Title = "Capturar " .. rarity,
+        Default = false,
+        Callback = function(state)
+            getgenv().AutoCatchConfig.TargetRarities[rarity] = state
+        end,
+    })
+end
+
+if getgenv().StartFarm then
+    getgenv().StartFarm(Notification)
+end
