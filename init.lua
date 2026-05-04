cf6f2af92c9b1726b734e8f20a3dee5035eb5eb3
pigsouza <vbenicio089@gmail.com>
2026-05-03 23:25:31 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 local githubUrl = "https://raw.githubusercontent.com/pigsouza/AutoCatch/main/"
 
--- Cria uma tabela global para os dois scripts se comunicarem
 getgenv().AutoCatchConfig = {
     AutoCatch = false,
     TargetRarities = {
@@ -10,8 +9,7 @@ getgenv().AutoCatchConfig = {
     }
 }
 
--- Baixa e executa os módulos
 loadstring(game:HttpGet(githubUrl .. "src/farm.lua"))()
 loadstring(game:HttpGet(githubUrl .. "src/ui.lua"))()
 
-print("⚡ Scripts carregados com sucesso!")
+print("O script foi carregado.")
