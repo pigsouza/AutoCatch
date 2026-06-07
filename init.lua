d824805186c86ca7e05a2909239382b3a826983c
pigsouza <vbenicio089@gmail.com>
2026-06-07 15:09:23 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,10 +1,16 @@
 local Players = game:GetService("Players")
 local VirtualUser = game:GetService("VirtualUser")
+
+Players.LocalPlayer.Idled:Connect(function()
+    VirtualUser:CaptureController()
+    VirtualUser:ClickButton2(Vector2.new())
+    print("Anti-AFK")
+end)
+
 local githubUrl = "https://raw.githubusercontent.com/pigsouza/AutoCatch/bot-dsc/"
 
 getgenv().AutoCatchConfig = getgenv().AutoCatchConfig or {
     AutoCatch = false,
-    AutoEgg = false,          -- 👈 ADICIONADO
     SecretLuckyBlockOnly = false,
     TargetRarities = {
         Common = false, Rare = false, Epic = false,
@@ -12,7 +18,10 @@ getgenv().AutoCatchConfig = getgenv().AutoCatchConfig or {
         Boss = false, Divine = false
     },
     Blink = {
-        Islands = { Cave = false, Safari = false },
+        Islands = {
+            Cave = false,
+            Safari = false
+        },
         TargetRarities = {
             Common = false, Rare = false, Epic = false,
             Legendary = false, Mythical = false, Secret = false,
@@ -21,8 +30,7 @@ getgenv().AutoCatchConfig = getgenv().AutoCatchConfig or {
     }
 }
 
-loadstring(game:HttpGet(githubUrl .. "egg.lua"))()   -- 👈 ADICIONADO
 loadstring(game:HttpGet(githubUrl .. "farm.lua"))()
 loadstring(game:HttpGet(githubUrl .. "ui.lua"))()
 
-print("Script carregado com sucesso.")
+print("O script foi carregado com sucesso (Anti-AFK ativado).")
